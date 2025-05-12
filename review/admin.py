8d59e869721d18a0ad7301bb6eb759c15a397caf
Surja11 <stha.surja123@gmail.com>
2025-05-12 10:25:31 +0545

    Updated admin.py

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 # Register your models here.
+
+class UserModelAdmin(UserAdmin):
+  model = User
+  list_display = ["id","email","username","is_superuser","is_staff"]
+  list_filter = ["is_superuser"]
+  fieldsets = [("User Credentials",{"fields": ["email", "password"]}),("Personal Information",{"fields":["username"]}),("Permissions",{"fields":["is_staff","is_superuser"]})]
+
+  add_fieldsets = [
+    (None, {
+      "classes": ["wide"],
+      "fields": ["email","password1", "password2"]
+    })
+  ]
+  filter_horizontal = []
+
+admin.site.register(User, UserModelAdmin)
