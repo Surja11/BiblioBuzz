65172bb19ac9dd9378742360ea873d4a29cf0841
Surja11 <stha.surja123@gmail.com>
2025-05-12 12:13:12 +0545

    Added template for register

---
+++ review/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import *
+
+
+class RegistrationForm(forms.ModelForm):
+  
+  password = forms.CharField(widget = forms.PasswordInput)
+  confirm_password = forms.CharField(widget = forms.PasswordInput)
+  class Meta:
+    model = User
+    fields = ["email","username","password","confirm_password"]
+
+  def clean(self):
+    cleaned_data = super().clean()
+    password = cleaned_data.get('password')
+    confirm_password = cleaned_data.get('confirm_password')
+
+    if password != confirm_password:
+      self.add_error('confirm_password','Password and Confirm Password do not match')
+      return cleaned_data
+    
+    def clean_email(self):
+      email = self.cleaned_data.get('email')
+      if User.objects.filter(email = email).exists():
+        raise forms.ValidationError("A user with this email already exists")
+      
+
+class LoginForm(forms.Form):
+  email = forms.EmailInput()
+  password = forms.PasswordInput()
+
+  fields=['email', 'password']
+
+
+
+
+
+
--- review/views.py
+++ review/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import *
 # Create your views here.
 def home(request):
   return render(request, 'review/home.html')
@@ -9,7 +10,20 @@ def reviewPage(request, slug):
   return render(request, 'review/requestPage.html')
 
 def userRegister(request):
-  return render(request, 'review/register.html')
+  if request.method == "POST":
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit = False)
+      user.set_password(form.cleaned_data["password"])
+      user.save()
+      messages.success("Registration successful")
+      return redirect('login')
+    
+  form = RegistrationForm()
+  return render(request, 'review/register.html',{'form': form})
 
 def userLogin(request):
-  return render(request, 'review/login.html')
+  if request.method == "POST":
+    pass
+  form = LoginForm()
+  return render(request, 'review/login.html',{'form': form})
